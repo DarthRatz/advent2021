009caaa1be2ba5f2db14b15b3252e1e8fedb2eb6
Éamonn Reilly <Eamonn.Reilly@liberty-it.co.uk>
2021-12-02 11:50:53 +0000

    day 2

---
+++ day2/avent02.py
@@ -0,0 +1,21 @@
+# f = open("day2/test02.txt", "r")
+f = open("day2/input02.txt", "r")
+lines = f.readlines()
+
+instructions = [tuple(line.split()) for line in lines]
+
+horizontal = 0
+aim = 0
+depth = 0
+for instruction in instructions:
+    if instruction[0] == 'forward':
+        horizontal += int(instruction[1])
+        depth += int(instruction[1]) * aim
+    if instruction[0] == 'down':
+        aim += int(instruction[1])
+    if instruction[0] == 'up':
+        aim -= int(instruction[1])
+
+print(depth)
+print(horizontal)
+print(depth*horizontal)
